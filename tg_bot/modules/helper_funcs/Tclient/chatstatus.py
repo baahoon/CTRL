907d148adede1d351e4d5de252d632767e32629d
Sourcery AI <>
2020-11-28 08:32:32 +0000

    'Refactored by Sourcery'

--- tg_bot/modules/helper_funcs/Tclient/chatstatus.py
+++ tg_bot/modules/helper_funcs/Tclient/chatstatus.py
@@ -4,59 +4,50 @@ from telethon.tl.types import ChannelParticipantsAdmins
 
 
 async def user_is_ban_protected(user_id: int, message):
-    status = False
     if message.is_private or user_id in (MOD_USERS):
         return True
 
-    async for user in Tclient.iter_participants(
-            message.chat_id, filter=ChannelParticipantsAdmins):
-        if user_id == user.id:
-            status = True
-            break
-    return status
+    return any(
+        user_id == user.id
+        for user in Tclient.iter_participants(
+            message.chat_id, filter=ChannelParticipantsAdmins
+        )
+    )
 
 
 async def user_is_admin(user_id: int, message):
-    status = False
     if message.is_private:
         return True
 
-    async for user in Tclient.iter_participants(
-            message.chat_id, filter=ChannelParticipantsAdmins):
-        if user_id == user.id or user_id in SUDO_USERS:
-            status = True
-            break
-    return status
+    return any(
+        user_id == user.id or user_id in SUDO_USERS
+        for user in Tclient.iter_participants(
+            message.chat_id, filter=ChannelParticipantsAdmins
+        )
+    )
 
 
 async def is_user_admin(user_id: int, chat_id):
-    status = False
-    async for user in Tclient.iter_participants(
-            chat_id, filter=ChannelParticipantsAdmins):
-        if user_id == user.id or user_id in SUDO_USERS:
-            status = True
-            break
-    return status
+    return any(
+        user_id == user.id or user_id in SUDO_USERS
+        for user in Tclient.iter_participants(
+            chat_id, filter=ChannelParticipantsAdmins
+        )
+    )
 
 
 async def ctrl_is_admin(chat_id: int):
-    status = False
     ctrl = await Tclient.get_me()
-    async for user in Tclient.iter_participants(
-            chat_id, filter=ChannelParticipantsAdmins):
-        if ctrl.id == user.id:
-            status = True
-            break
-    return status
+    return any(
+        ctrl.id == user.id
+        for user in Tclient.iter_participants(
+            chat_id, filter=ChannelParticipantsAdmins
+        )
+    )
 
 
 async def is_user_in_chat(chat_id: int, user_id: int):
-    status = False
-    async for user in Tclient.iter_participants(chat_id):
-        if user_id == user.id:
-            status = True
-            break
-    return status
+    return any(user_id == user.id for user in Tclient.iter_participants(chat_id))
 
 
 async def can_change_info(message):
@@ -99,7 +90,6 @@ async def can_delete_messages(message):
     if message.is_private:
         return True
     elif message.chat.admin_rights:
-        status = message.chat.admin_rights.delete_messages
-        return status
+        return message.chat.admin_rights.delete_messages
     else:
         return False
